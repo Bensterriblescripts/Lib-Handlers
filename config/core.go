b47fe71ba589e711952271f324024bf81d17c41a
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-11-08 10:17:56 +1300

    fixed up vars for the config path

--- config/core.go
+++ config/core.go
@@ -12,6 +12,29 @@ import (
 
 var CurrentConfiguration map[string]string
 
+func ReadConfig() map[string]string {
+	rawconfig := GetConfig()
+
+	out := make(map[string]string)
+	s := bufio.NewScanner(bytes.NewReader(rawconfig))
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		i := strings.IndexRune(line, '=')
+		if i < 0 {
+			continue
+		}
+		k := strings.TrimSpace(line[:i])
+		v := strings.TrimSpace(line[i+1:])
+		if len(v) >= 2 && ((v[0] == '"' && v[len(v)-1] == '"') || (v[0] == '\'' && v[len(v)-1] == '\'')) {
+			v = v[1 : len(v)-1]
+		}
+		out[k] = v
+	}
+	return out
+}
 func GetConfig() []byte {
 	if !osapi.EnsurePath(ConfigPath) {
 		ErrorLog("Failed to retrieve the config during directory creation")
@@ -40,44 +63,12 @@ func GetConfig() []byte {
 		return filecontent
 	}
 }
-
-func ReadConfig(config []byte) map[string]string {
-	out := make(map[string]string)
-	s := bufio.NewScanner(bytes.NewReader(config))
-	for s.Scan() {
-		line := strings.TrimSpace(s.Text())
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-		i := strings.IndexRune(line, '=')
-		if i < 0 {
-			continue
-		}
-		k := strings.TrimSpace(line[:i])
-		v := strings.TrimSpace(line[i+1:])
-		if len(v) >= 2 && ((v[0] == '"' && v[len(v)-1] == '"') || (v[0] == '\'' && v[len(v)-1] == '\'')) {
-			v = v[1 : len(v)-1]
-		}
-		out[k] = v
-	}
-	return out
-}
 func OverwriteConfig(newconfigmap map[string]string) {
 	if !osapi.EnsurePath(ConfigPath) {
 		ErrorLog("Failed to create the config directory")
 		return
 	}
-
-	currentconfig := GetConfig()
-	if currentconfig == nil {
-		if !WriteConfig(ConfigPath, newconfigmap) {
-			ErrorLog("Failed to write the config file")
-			return
-		} else {
-			TraceLog("Wrote to the config file")
-		}
-	}
-	currentconfigmap := ReadConfig(currentconfig)
+	currentconfigmap := ReadConfig()
 	for key, _ := range currentconfigmap {
 		if _, exists := newconfigmap[key]; exists {
 			currentconfigmap[key] = newconfigmap[key]
@@ -86,18 +77,18 @@ func OverwriteConfig(newconfigmap map[string]string) {
 		}
 	}
 
-	if !WriteConfig(ConfigPath, currentconfigmap) {
+	if !WriteConfig(currentconfigmap) {
 		ErrorLog("Failed to write the config file")
 		return
 	} else {
 		TraceLog("Wrote to the config file")
 	}
 }
-func WriteConfig(path string, configmap map[string]string) bool {
+func WriteConfig(configmap map[string]string) bool {
 	var buffer bytes.Buffer
 	for key, value := range configmap {
 		buffer.WriteString(key + "=" + value + "\n")
-		if ErrExists(os.WriteFile(path, buffer.Bytes(), 0644)) {
+		if ErrExists(os.WriteFile(ConfigPath, buffer.Bytes(), 0644)) {
 			ErrorLog("Failed to write the config file")
 			return false
 		}
