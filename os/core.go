c30c17eb98f1a621bd94ca2f49ff8494ed6a97e4
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-11-11 17:59:30 +1300

    added a hide window for applications that dont use an existing console as stdout

--- os/core.go
+++ os/core.go
@@ -14,21 +14,20 @@ func HideConsole(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 }
 func Run(command string) (string, bool) {
+	var cmd *exec.Cmd
+
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command("powershell.exe", "-Command", command)
-		if out, err := ErrorExists(cmd.CombinedOutput()); err {
-			return string(out), false
-		} else {
-			return string(out), true
-		}
+		cmd = exec.Command("powershell.exe", "-NoProfile", "-NonInteractive", "-Command", command)
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true} // No powershell window
 	} else {
-		cmd := exec.Command("bash", "-c", command)
-		if out, err := ErrorExists(cmd.CombinedOutput()); err {
-			return string(out), false
-		} else {
-			return string(out), true
-		}
+		cmd = exec.Command("bash", "-c", command)
 	}
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(out), false
+	}
+	return string(out), true
 }
 func EnsurePath(path string) bool {
 	if ErrExists(os.MkdirAll(filepath.Dir(path), 0755)) {
