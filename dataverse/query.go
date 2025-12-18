3d2b9bcdea856d27eecb6923078de5b10e60085f
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-12-19 05:35:02 +1300

    separated queries by id and params, added returnvalues param

--- dataverse/query.go
+++ dataverse/query.go
@@ -15,10 +15,71 @@ import (
 // - Use dataverse.Authenticate() to retrieve a new token
 //
 // Usage: dataverse.Request("contacts", "contactid eq 1234567890")
-func Request(table string, params string) []byte {
+func Request(table string, params string, returnvalues string) []byte {
 	url := fmt.Sprintf("%s/api/data/v9.2/%s", Endpoint, table)
-	if params != "" {
+	if returnvalues != "" && params == "" {
+		url += "?$select=" + returnvalues
+	} else if returnvalues != "" && params != "" {
+		url += "?" + params + "&$select=" + returnvalues
+	} else if returnvalues == "" && params != "" {
 		url += "?" + params
+	}
+
+	if req, err := ErrorExists(http.NewRequest("GET", url, nil)); err { // Create request
+		ErrorLog("Failed to create request: " + url)
+		return nil
+	} else {
+		req.Header.Set("Authorization", "Bearer "+CurrentAccessToken.AccessToken)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("OData-MaxVersion", "4.0")
+		req.Header.Set("OData-Version", "4.0")
+
+		client := &http.Client{}
+		if resp, err := ErrorExists(client.Do(req)); err { // Send request
+			ErrorLog("CRM Request Failed: " + url)
+			return nil
+		} else {
+			defer resp.Body.Close()
+
+			if NetworkDebug {
+				TraceLog("Sending request...  " + url + " HTTP Status: " + strconv.Itoa(resp.StatusCode))
+				TraceLog("----------")
+				TraceLog("Response Headers:")
+				for key, value := range resp.Header {
+					TraceLog(fmt.Sprintf("%s: %s", key, value))
+				}
+				TraceLog("----------")
+			}
+
+			if body, err := ErrorExists(io.ReadAll(resp.Body)); err { // Read response
+				ErrorLog("Failed to read response body")
+				return nil
+			} else {
+				if NetworkDebug {
+					TraceLog("Response body:")
+					TraceLog(string(body))
+					TraceLog("----------")
+				}
+				if resp.StatusCode != 200 {
+					ErrorLog(fmt.Sprintf("HTTP Error %d: %s", resp.StatusCode, string(body)))
+					return nil
+				}
+				return body
+			}
+		}
+	}
+}
+
+// Requires an existing access token in dataverse.CurrentAccessToken
+//
+// - Use dataverse.Authenticate() to retrieve a new token
+//
+// Usage: dataverse.Retrieve("contacts", "1234567890", "fullname,lastname,emailaddress1")
+func Retrieve(table string, id string, returnvalues string) []byte {
+	url := fmt.Sprintf("%s/api/data/v9.2/%s(%s)", Endpoint, table, id)
+	if returnvalues != "" {
+		url += "?$select=" + returnvalues
 	}
 
 	if req, err := ErrorExists(http.NewRequest("GET", url, nil)); err { // Create request
