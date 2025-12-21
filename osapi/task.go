3497115715ac710f17beb773b70381ccfa2ebf3c
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2025-12-22 10:48:12 +1300

    changed task name and function names to be more concise/clear

--- osapi/task.go
+++ osapi/task.go
@@ -4,14 +4,16 @@ import (
 	. "github.com/Bensterriblescripts/Lib-Handlers/logging"
 )
 
-func RunExecutableAtLogonRegistry(name string, path string) {
+func RunExeAtLogon(name string, path string) bool {
 	out, success := Run(`
 		New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "` + name + `" -Value '"` + path + `"' -PropertyType String -Force
 	`)
 	if !success {
 		ErrorLog("Failed to create task: " + name)
 		ErrorLog(out)
+		return false
 	} else {
 		TraceLog("Created new executable task at logon: " + name)
+		return true
 	}
 }
